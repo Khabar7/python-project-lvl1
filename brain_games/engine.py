a3da1a1ee2f0b0b0788a58752778593a035f24df
khabar7 <khabarov.ilya7@yandex.ru>
2022-07-13 21:35:37 +0400

    refactoring engine() part 4

--- brain_games/engine.py
+++ brain_games/engine.py
@@ -5,6 +5,8 @@ NUMBER_OF_ROUNDS = 3
 
 def engine(game=None):
     user_name = welcome_user(game)
+    if not game:
+        return
     correct_answers = 0
     while correct_answers < NUMBER_OF_ROUNDS:
         question, right = game.make_question()
